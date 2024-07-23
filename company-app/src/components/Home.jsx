01cd6198626045d5501df817d23289e21fdd4de6
Ankush Maheshwari <ankush97m@gmail.com>
2024-07-23 18:07:33 -0400

    display company names on homepage using mock api

--- company-app/src/components/Home.jsx
+++ company-app/src/components/Home.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { List, ListItem, ListItemText, Container } from "@mui/material";
 
 export default function Home() {
+  const [companies, setCompanies] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Mock API url
+    const apiUrl = "https://jsonplaceholder.typicode.com/users";
+
+    // function to fetch data of all the companies
+    const fetchAllCompanies = async () => {
+      try {
+        const res = await fetch(apiUrl);
+        if (!res.ok) {
+          throw new Error("500 Service Error");
+        }
+        const data = await res.json();
+        setCompanies(data);
+      } catch (error) {
+        setError(error);
+      } 
+    };
+
+    fetchAllCompanies();
+  }, []);
+
   return (
-    <div>Home</div>
-  )
+    <>
+        <h2>Companies</h2>
+        <Container>
+            <List>
+                {companies.map((company) => (
+                <ListItem key={company.id}>
+                    <ListItemText primary={company.name} secondary={company.email} />
+                </ListItem>
+                ))}
+            </List>
+        </Container>
+    </>
+  );
 }
