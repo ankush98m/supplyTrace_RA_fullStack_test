42e1fb574e2472bdb5acfa9e93c6605e2dea4392
Ankush Maheshwari <ankush97m@gmail.com>
2024-07-23 18:28:41 -0400

    send company id by using useNavigate hook

--- company-app/src/components/CompanyDetails.jsx
+++ company-app/src/components/CompanyDetails.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export default function CompanyDetails() {
+    const { id } = useParams();
   return (
-    <div>CompanyDetails</div>
+    <div>
+        <h2>Company Details</h2>
+        <h5> {id}</h5>
+    </div>
   )
 }
--- company-app/src/components/Home.jsx
+++ company-app/src/components/Home.jsx
@@ -1,39 +1,44 @@
 import React, { useEffect, useState } from "react";
-import { List, ListItem, ListItemText, Container } from "@mui/material";
+import { List, ListItem, ListItemText, Container, Button } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
+import CompanyDetails from "./CompanyDetails";
 
 export default function Home() {
   const [companies, setCompanies] = useState([]);
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  // Mock API url
+  const apiUrl = "https://jsonplaceholder.typicode.com/users";
+
+  // function to fetch data of all the companies
+  const fetchAllCompanies = async () => {
+    try {
+      const res = await fetch(apiUrl);
+      if (!res.ok) {
+        throw new Error("500 Service Error");
+      }
+      const data = await res.json();
+      setCompanies(data);
+    } catch (error) {
+      setError(error);
+    } 
+  };
 
   useEffect(() => {
-    // Mock API url
-    const apiUrl = "https://jsonplaceholder.typicode.com/users";
-
-    // function to fetch data of all the companies
-    const fetchAllCompanies = async () => {
-      try {
-        const res = await fetch(apiUrl);
-        if (!res.ok) {
-          throw new Error("500 Service Error");
-        }
-        const data = await res.json();
-        setCompanies(data);
-      } catch (error) {
-        setError(error);
-      } 
-    };
-
     fetchAllCompanies();
   }, []);
 
   return (
     <>
         <h2>Companies</h2>
-        <Container>
+        <Container> 
             <List>
                 {companies.map((company) => (
                 <ListItem key={company.id}>
-                    <ListItemText primary={company.name} secondary={company.email} />
+                    <Button onClick={() => navigate(`/${company.id}`)}>
+                        <ListItemText primary={company.name} secondary={company.email} />
+                    </Button>
                 </ListItem>
                 ))}
             </List>
