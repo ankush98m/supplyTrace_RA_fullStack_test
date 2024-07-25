937293f8337329470eee0e8e2257931ffc1f306f
Ankush Maheshwari <ankush97m@gmail.com>
2024-07-25 00:11:48 -0400

    create locations model

--- server/companies/models.py
+++ server/companies/models.py
@@ -9,3 +9,13 @@ class Companies(models.Model):
 
     def __str__(self):
         return self.name
+
+class Locations(models.Model):
+    company_id = models.ForeignKey(Companies, on_delete=models.CASCADE, db_column='company_id')
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
